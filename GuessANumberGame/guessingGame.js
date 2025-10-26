53da3c798c8aadb6963e725fb791ba3062f2df63
Jasiah <jasiahodell4@gmail.com>
2025-10-26 16:04:34 -0700

    added comments

---
+++ GuessANumberGame/guessingGame.js
@@ -0,0 +1,7 @@
+/*
+Jasiah Odell
+Full Stack Web Development, Fall 2025
+Project 3: Guess-a-number Game
+Description: A webpage that lets the user play a guess-a-number game. Using html to create a UI, css for styling, and js for functionality.
+*/
+
