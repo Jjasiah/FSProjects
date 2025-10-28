208cc2f94a1339a1451d59a7f588022603bfffa4
Jasiah <jasiahodell4@gmail.com>
2025-10-28 13:38:31 -0700

    added whack-a-mole mini-project

---
+++ Whack/whack_starter.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+
+// TODO 1: select the grid, score, and misses elements
+// const grid = ...
+// const scoreEl = ...
+// const missesEl = ...
+
+// TODO 2: write a randomizeMole() function
+// Hints: use document.querySelectorAll("#grid button");
+// loop through them and remove "mole"
+// then pick one random index and add "mole"
+
+// TODO 3: add a click event listener to grid
+// Inside, check if e.target is a button with class "mole"
+// If yes: increment score, call randomizeMole()
+// Otherwise: increment misses
+
+// TODO 4: call randomizeMole() and start setInterval(randomizeMole, 1500)
+
+}
